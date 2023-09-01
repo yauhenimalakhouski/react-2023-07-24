706be621395edd299e8149d683e9213dfd5cc03d
yauhenimalakhouski <122909276+yauhenimalakhouski@users.noreply.github.com>
2023-09-01 15:53:32 +0300

    files deleted

--- src/store/features/review/selectors.js
+++
@@ -1,6 +0,0 @@
-export const selectReviewModule = (state) => state.review;
-
-export const selectReviewById = (state, id) =>
-  selectReviewModule(state).entities[id];
-
-export const selectReviewIds = (state) => selectReviewModule(state).ids;
