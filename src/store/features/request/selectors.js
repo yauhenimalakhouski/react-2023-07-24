175e9343f5481b894668231b9522523ef19ee3d4
yauhenimalakhouski <122909276+yauhenimalakhouski@users.noreply.github.com>
2023-09-01 15:51:05 +0300

    files deleted

--- src/store/features/request/selectors.js
+++
@@ -1,6 +0,0 @@
-import { LOADING_STATUS } from "../../../constants/loading-statuses";
-
-export const selectRequestModule = (state) => state.request;
-
-export const selectRequestStatus = (state, requestId) =>
-  selectRequestModule(state)[requestId]?.status || LOADING_STATUS.idle;
--- src/store/index.js
+++ src/store/index.js
@@ -1,24 +1,14 @@
 import { cartSlice } from "./features/cart";
-import { restaurantSlice } from "./features/restaurant";
 import { loggerMiddleware } from "./middlewares/logger";
-import { dishSlice } from "./features/dish";
-import { reviewSlice } from "./features/review";
-import { userSlice } from "./features/user";
 import { authorizationSlice } from "./features/authorization";
 import { checkAuthorization } from "./middlewares/check-authorization";
-import { requestSlice } from "./features/request";
 import { configureStore } from "@reduxjs/toolkit";
 import { api } from "./services/api";
 
 export const store = configureStore({
   reducer: {
     cart: cartSlice.reducer,
-    restaurant: restaurantSlice.reducer,
-    dish: dishSlice.reducer,
-    review: reviewSlice.reducer,
-    user: userSlice.reducer,
     authorization: authorizationSlice.reducer,
-    request: requestSlice.reducer,
     [api.reducerPath]: api.reducer,
   },
   middleware: (getDefaultMiddleware) => [
